65e1d56778069bed9ff987235f30ca283ec283ed
trevormac <trevor.c.macgregor@gmail.com>
2017-04-20 18:13:38 -0400

    added article class

---
+++ Nusic/Article.swift
@@ -0,0 +1,20 @@
+//
+//  Article.swift
+//  Nusic
+//
+//  Created by Trevor MacGregor on 2017-04-20.
+//  Copyright © 2017 Nusic_Inc. All rights reserved.
+//
+
+import Foundation
+
+class Article: NSObject {
+    
+    var articleTitle: String?
+    var articleSummary: String?
+    var articleURL: String?
+    var articleImage: String?
+    var articleSourceTitle: String?
+    
+    
+}
