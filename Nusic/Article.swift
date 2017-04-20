b2c9986bfcbe90e4f4b7d7025d4e6d1f4bb93379
Rapier97 <alex.rapier13@gmail.com>
2017-04-20 18:14:00 -0400

    tvc added

---
+++ Nusic/Article.swift
@@ -0,0 +1,20 @@
+
+
+
+
+//
+//  Article.swift
+//  Nusic
+//
+//  Created by Alex Rapier on 20/04/2017.
+//  Copyright © 2017 Nusic_Inc. All rights reserved.
+//
+
+import UIKit
+
+class Article: NSObject {
+    
+    var headline = <#value#>
+    
+
+}
