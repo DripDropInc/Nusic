2cde57d55dfdc1ea49cb8be75933cafb8c56f6f0
Rapier97 <alex.rapier13@gmail.com>
2017-04-20 18:30:33 -0400

    storyboard

--- Nusic/Article.swift
+++ Nusic/Article.swift
@@ -1,27 +1,9 @@
-<<<<<<< HEAD
 
-
-
-
-=======
->>>>>>> 65e1d56778069bed9ff987235f30ca283ec283ed
 //
 //  Article.swift
 //  Nusic
 //
-<<<<<<< HEAD
-//  Created by Alex Rapier on 20/04/2017.
-//  Copyright © 2017 Nusic_Inc. All rights reserved.
 //
-
-import UIKit
-
-class Article: NSObject {
-    
-    var headline = <#value#>
-    
-
-=======
 //  Created by Trevor MacGregor on 2017-04-20.
 //  Copyright © 2017 Nusic_Inc. All rights reserved.
 //
@@ -36,6 +18,4 @@ class Article: NSObject {
     var articleImage: String?
     var articleSourceTitle: String?
     
-    
->>>>>>> 65e1d56778069bed9ff987235f30ca283ec283ed
 }
