b6fde2daf8015ad1ce8fc0918eed694e612bf08a
trevormac <trevor.c.macgregor@gmail.com>
2017-04-20 12:55:01 -0400

    Added Artist Request Class.

---
+++ Nusic/ArtistRequest.swift
@@ -0,0 +1,106 @@
+//
+//  RequestController.swift
+//  Nusic
+//
+//  Created by Trevor MacGregor on 2017-04-19.
+//  Copyright © 2017 none. All rights reserved.
+//
+
+import Foundation
+
+
+class RequestController {
+    func sendRequest() {
+        let sessionConfig = URLSessionConfiguration.default
+        
+        /* Create session, and optionally set a URLSessionDelegate. */
+        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        
+        /* Create the Request:
+         Request (GET https://music-api.musikki.com/v1/artists/100052041/news)
+         */
+        
+        guard var URL = URL(string: "https://music-api.musikki.com/v1/artists/") else {return}
+        let URLParams = ["q": "mastadon",]
+        URL = URL.appendingQueryParameters(URLParams)
+        var request = URLRequest(url: URL)
+        request.httpMethod = "GET"
+        
+        // Headers
+        
+        request.addValue("12a985889510737104d84141c9f79232", forHTTPHeaderField: "Appid")
+        request.addValue("ae1bc12c9434b2bd3cf9e15242f08be8", forHTTPHeaderField: "Appkey")
+        
+        /* Start a new Task */
+        let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            if (error == nil) {
+                // Success
+                let statusCode = (response as! HTTPURLResponse).statusCode
+                print("URL Session Task Succeeded: HTTP \(statusCode)")
+                
+                // Check if data is returned
+                if let data = data {
+                    guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] else {
+                        print("data returned is not json, or not valid")
+                        return
+                    }
+                    //                    print(json!)
+                    
+                           if let results = json!["results"] as? [[String: AnyObject]] {
+                        print("_________________________")
+                            print(results)
+                    }
+                }
+            }
+            else {
+                // Failure
+                print("URL Session Task Failed: %@", error!.localizedDescription);
+            }
+        })
+        
+        task.resume()
+        session.finishTasksAndInvalidate()
+    }
+}
+
+
+protocol URLQueryParameterStringConvertible {
+    var queryParameters: String {get}
+}
+
+extension Dictionary : URLQueryParameterStringConvertible {
+    /**
+     This computed property returns a query parameters string from the given NSDictionary. For
+     example, if the input is @{@"day":@"Tuesday", @"month":@"January"}, the output
+     string will be @"day=Tuesday&month=January".
+     @return The computed parameters string.
+     */
+    var queryParameters: String {
+        var parts: [String] = []
+        for (key, value) in self {
+            let part = String(format: "%@=%@",
+                              String(describing: key).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!,
+                              String(describing: value).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+            parts.append(part as String)
+        }
+        return parts.joined(separator: "&")
+    }
+    
+}
+
+extension URL {
+    /**
+     Creates a new URL by adding the given query parameters.
+     @param parametersDictionary The query parameter dictionary to add.
+     @return A new URL.
+     */
+    func appendingQueryParameters(_ parametersDictionary : Dictionary<String, String>) -> URL {
+        let URLString : String = String(format: "%@?%@", self.absoluteString, parametersDictionary.queryParameters)
+        return URL(string: URLString)!
+    }
+    
+    
+
+}
+
+
